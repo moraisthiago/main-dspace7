87c373003db2040781d666e5671408f92925e76d
moraisthiago <thiagueson@gmail.com>
2022-12-15 10:04:18 -0300

    Configurando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,23 @@
 import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RodapeComponent } from './rodape/rodape.component';
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main/main.component';
+import { RodapeComponent } from './rodape/rodape.component';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    RodapeComponent,
-    HeaderComponent
-  ],
+  declarations: [AppComponent, RodapeComponent, HeaderComponent, MainComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatGridListModule,
-    MatCardModule,
     MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
